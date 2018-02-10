e149b8b691cfefdec2d545336bd4a054f555975e
Maxime <Maxime@DESKTOP-KFVPR0A>
2018-02-10 17:02:52 +0100

    Ajoutez des fichiers projet.

---
+++ Ideogen.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ideogen.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
